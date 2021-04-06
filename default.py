3f1bce8e56a18c5cefbc17bc97e2a0376b56c40b
archiron <arnaud.chiron@cern.ch>
2021-04-06 14:38:39 +0200

    begin corrections into default.py file

--- default.py
+++ default.py
@@ -4,12 +4,22 @@
 import os,sys
 
 #############################################################################
-# fixed data
-tmp_path = '/eos/project/c/cmsweb/www/egamma/validation/Electrons/quickValidationSeq/HistosConfigFiles/'
+# fixed data for ChiLib library
+#tmp_path = '/eos/project/c/cmsweb/www/egamma/validation/Electrons/quickValidationSeq/HistosConfigFiles/'
 image_up = "http://cms-egamma.web.cern.ch/validation/Electrons/img/up.gif"
 image_point = "http://cms-egamma.web.cern.ch/validation/Electrons/img/point.gif"
 image_OK = "http://cms-egamma.web.cern.ch/validation/Electrons/img/OK.gif"
 image_KO = "http://cms-egamma.web.cern.ch/validation/Electrons/img/KO.gif"
+
+KS_path_local = '/eos/project/c/cmsweb/www/egamma/validation/Electrons/Store/KS_Curves/'
+KS_path_web = 'http://cms-egamma.web.cern.ch/validation/Electrons/Store/KS_Curves/'
+
+# fixed data for git clone library
+tmp_path = '/ChiLib_CMS_Validation/HistosConfigFiles/'
+#image_up = "../ChiLib_CMS_Validation/img/up.gif"
+#image_point = "../ChiLib_CMS_Validation/img/point.gif"
+#image_OK = "../ChiLib_CMS_Validation/img/OK.gif"
+#image_KO = "../ChiLib_CMS_Validation/img/KO.gif"
 
 KS_path_local = '/eos/project/c/cmsweb/www/egamma/validation/Electrons/Store/KS_Curves/'
 KS_path_web = 'http://cms-egamma.web.cern.ch/validation/Electrons/Store/KS_Curves/'
