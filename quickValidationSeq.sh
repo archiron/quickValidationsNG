a37b02abbea1ec2ee6e12b3ed92cb048ed4e3a09
archiron <arnaud.chiron@cern.ch>
2021-04-09 15:01:32 +0200

    version 2021.04.09-15:01
    modification of the quickValidationSeq.sh to run without stops.

--- quickValidationSeq.sh
+++ quickValidationSeq.sh
@@ -6,8 +6,8 @@ echo "LOG_SOURCE : $LOG_SOURCE"
 
 git clone https://github.com/archiron/quickValidationsNG quickValidationsNG
 git clone https://github.com/archiron/ChiLib_CMS_Validation ChiLib_CMS_Validation
+# the config.py file is assumed to be modified.
+cp config.py quickValidationsNG/config.py
 cd quickValidationsNG/
-# preparation of the config.py file (see later)
-# cp config.py.test config.py
-echo ls
+
 python mainSeq.py
