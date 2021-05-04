e9bfffe14f2da479914374ae0fbbef0d84f4a5e6
archiron <arnaud.chiron@cern.ch>
2021-05-04 14:27:15 +0200

    version 2021.05.04
    modification of the sh script in order to clone folders and preinstall the config.py file.

--- quickValidationSeq.sh
+++ quickValidationSeq.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # This file is called ./validations.sh
 #
-LOG_SOURCE=$PWD
-echo "LOG_SOURCE : $LOG_SOURCE"
 
 git clone https://github.com/archiron/quickValidationsNG quickValidationsNG
 git clone https://github.com/archiron/ChiLib_CMS_Validation ChiLib_CMS_Validation
 # the config.py file is assumed to be modified.
-cp config.py quickValidationsNG/config.py
 cd quickValidationsNG/
+cp config.py.test config.py
 
-python mainSeq.py
